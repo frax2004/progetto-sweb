a9ac3be4edfe225f33d2299305cf7a5090d92436
Giovanni <nannibutera08@gmail.com>
2026-05-19 19:39:20 +0200

    Inizio costruzione navbar

--- dndhub/src/app/components/navbar/navbar.component.ts
+++ dndhub/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { IonToolbar, IonTitle } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  imports: [IonToolbar, IonTitle],
 })
 export class NavbarComponent  implements OnInit {
+  @Input() Elements: 
+  @Input() title: String | undefined;
+
 
   constructor() { }
 

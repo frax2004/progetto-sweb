574e823fda41afa580ce2a8dc83d4e495b4275a3
Giovanni <nannibutera08@gmail.com>
2026-05-20 16:23:16 +0200

    Aggiunzione di componente entry

---
+++ dndhub/src/app/components/entry/entry.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IonInput, IonItem } from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-entry',
+  templateUrl: './entry.component.html',
+  styleUrls: ['./entry.component.scss'],
+  imports: [IonInput, IonItem],
+})
+export class EntryComponent  implements OnInit {
+  @Input() label?: String;
+  @Input() labelPlacement?: String;
+  @Input() type?: String;
+  @Input() placeholder?: String;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

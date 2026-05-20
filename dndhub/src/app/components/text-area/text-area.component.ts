52cfa31208edb68367893c1b99e562443d4ea649
Giovanni <nannibutera08@gmail.com>
2026-05-20 16:28:42 +0200

    Aggiunta componente TextArea

---
+++ dndhub/src/app/components/text-area/text-area.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IonTextarea } from "@ionic/angular/standalone";
+
+@Component({
+  selector: 'app-text-area',
+  templateUrl: './text-area.component.html',
+  styleUrls: ['./text-area.component.scss'],
+  imports: [IonTextarea],
+})
+export class TextAreaComponent  implements OnInit {
+  @Input() fill?: String;
+  @Input() label?: String;
+  @Input() labelPlacement?: String;
+  @Input() helperText?: String;
+  @Input() errorText?: String;
+  @Input() placeholder?: String;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}

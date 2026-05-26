d853c55a37d88bc688ad352854c6416165445217
Giovanni <nannibutera08@gmail.com>
2026-05-26 17:09:53 +0200

    initial implementation of stats selection

--- dndhub/src/app/pages/Character creation pipeline/stats-selection/stats-selection.page.ts
+++ dndhub/src/app/pages/Character creation pipeline/stats-selection/stats-selection.page.ts
@@ -3,19 +3,64 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonGrid, IonRow, IonCol, IonLabel } from '@ionic/angular/standalone';
 import { RadioButtonComponent } from "src/app/components/radio-button/radio-button.component";
+import { ButtonComponent } from "src/app/components/button/button.component";
 
 @Component({
   selector: 'app-stats-selection',
   templateUrl: './stats-selection.page.html',
   styleUrls: ['./stats-selection.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonGrid, IonRow, RadioButtonComponent, IonCol, IonLabel]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonGrid, IonRow, RadioButtonComponent, IonCol, IonLabel, ButtonComponent]
 })
 export class StatsSelectionPage implements OnInit {
-  groupValue: String = 'pageRadio';
-  radioPersonalChoice = [
-    { listElementValue: 'radioPersonalChoice', text: '', class: 'pageRadio'},
-  ];
+  manualSelection: Boolean = true;
+  stdArray: Boolean = false;
+  randomSelection: Boolean = false;
+
+  // da fare mi raccomando
+  buttonCallbacks = {
+    manual: { onClick: () => {
+      this.manualSelection=true;
+      this.stdArray=false;
+      this.randomSelection=false;
+      } 
+    },
+
+    stdArr: { onClick: () => {
+      this.manualSelection=false;
+      this.stdArray=true;
+      this.randomSelection=false;
+      } 
+    },
+
+    random: { onClick: () => {
+      this.manualSelection=false;
+      this.stdArray=false;
+      this.randomSelection=true;
+      } 
+    },
+  };
+
+
+  radios = [
+    { listElementValue: 'Array1', text: 'Array1: bla bla bòa', class: 'radio'},
+    { listElementValue: 'Array2', text: 'Array2: bla bla bòa', class: 'radio'},
+  ]
+
+  getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
+  rollStats(): string {
+    let arr: number[] = [this.getRandomInt(6) + 1, this.getRandomInt(6) + 1, this.getRandomInt(6) + 1, this.getRandomInt(6) + 1];
+    let min = Math.min(...arr);
+    let sum = 0;
+    for (let i = 0; i<4; i++) {
+      sum += arr[i];
+    }
+    sum -= min;
+    return sum.toString();
+  }
 
 
   constructor() { }

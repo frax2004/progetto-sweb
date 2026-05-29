bfad1e4d891a96fed1960c9d4ad9ddf454f36dd4
Giovanni <nannibutera08@gmail.com>
2026-05-29 17:42:06 +0200

    first schema

--- dndhub/dbserver/schemas/5e-SRD-Damage-Types.sql
+++ dndhub/dbserver/schemas/5e-SRD-Damage-Types.sql
@@ -1,3 +1,6 @@
-create table (
-    
+create table if not exists DamageTypes (
+    index: text not null primary key;
+    name: text not null;
+    description: text not null;
+    url: text not null;
 )

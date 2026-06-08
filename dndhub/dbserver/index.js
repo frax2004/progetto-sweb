97995da7714c50dc2011c7d65deb607800fef679
Francesco Pollarà <PolarFrancy@gmail.com>
2026-06-08 11:00:57 +0200

    minor fix

--- dndhub/dbserver/index.js
+++ dndhub/dbserver/index.js
@@ -19,35 +19,10 @@ app.use("/api/auth", authRouter);
 // probabilmente ci andrà un middleware di autenticazione
 app.use("/api/user-utilities", userUtilitiesRouter);
 app.use("/api/character-management", characterManagementRouter)
-
-// per impostare la formattazione a 2 spazi di indentazione
-app.set('json spaces', 2);
-
-
-
-function loadTable(name) {
-  const text = fs.readFileSync(
-    DatabasePaths.DATA_DIR + name + ".json", 
-    'utf8'
-  );
-
-  const ObjToString = (x) => {
-    return "'" + x.toString().split(/['"]/).join("") + "'";
-  };
-
-  const query = JSON
-  .parse(text)
-  .map(obj => {""
-    const keys = Object.keys(obj).join(", ");
-    const values = Object.values(obj).map(ObjToString).join(", ");
-    return `INSERT OR IGNORE INTO ${name} (${keys}) VALUES (${values})`;
-  })
-  .join("; ");
-}
-
-// per impostare la formattazione a 2 spazi di indentazione
-app.set('json spaces', 2);
 app.use('/api/campagna', campagnaRouter);
+
+// per impostare la formattazione a 2 spazi di indentazione
+app.set('json spaces', 2);
 
 
 function loadTable(name) {

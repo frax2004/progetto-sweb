7deab43f8289d40f082b40fe6cfc963fadb76fc5
Giovanni <nannibutera08@gmail.com>
2026-06-14 14:43:58 +0200

    Revert "Merge branch 'Riccardo_Components' into giovanniDatabase"
    This reverts commit 96a7ee7045d70c8eb5fef5a3756572db5b1452f4, reversing
    changes made to 9ce1b6ce6ac6863cf40295f18c64df2c4a9268ba.

--- dndhub/dbserver/routes/post.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { PostsController } from "../controller/posts.controller.js";
-
-export const postRoutes = express.Router();
-
-
-postRoutes.get("/campagne/:idx_campagna/posts", PostsController.getPosts);
-postRoutes.post("/campagne/:idx_campagna/posts", PostsController.createPost);

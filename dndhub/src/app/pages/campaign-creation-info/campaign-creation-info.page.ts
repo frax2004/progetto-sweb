c1341a3525225f42c1a14d1db244a1e464a13c78
Giovanni <nannibutera08@gmail.com>
2026-06-22 22:50:09 +0200

    sistemazione di un paio di pagine

--- dndhub/src/app/pages/campaign-creation-info/campaign-creation-info.page.ts
+++ dndhub/src/app/pages/campaign-creation-info/campaign-creation-info.page.ts
@@ -17,16 +17,34 @@ export class CampaignCreationInfoPage implements OnInit {
 
   @Input() cards: Card[] = [
     {
-      imageURL: 'https://i.pinimg.com/1200x/3f/59/9b/3f599be10be14718b1cf5f0fb087f69f.jpg',
-      title: "Il mondo di Dungeons & Dragons è pieno di avventure...",
-      subtitle: "Non vorrai perdertele mica tutte!",
-      content: "Crea la tua campagna e potrai sbloccare infinite possibilità..."
+      imageURL: 'https://i.pinimg.com/1200x/31/c0/ac/31c0ac24049ddb8691569e4c3036252d.jpg',
+      title: "Create a campaign... ",
+      subtitle: " ...to connect with your friends!",
+      content: ""
     },
     {
-      imageURL: 'https://i.pinimg.com/736x/f0/2c/72/f02c72a7cb29a8ceefc8fcea10f706c2.jpg',
-      title: "Immagina raccontare una storia e giocarla...",
-      subtitle: "Come vivere una favola!",
-      content: "Raduna i tuoi amici e affrontate orripilanti mostri nelle segrete più oscure della fantasia!"
+      imageURL: 'https://i.pinimg.com/1200x/e6/99/06/e699060b283cbbb93c08ca270fa0d9f7.jpg',
+      title: "What even is a campaign... ",
+      subtitle: " ...in Dungeons & Dragons",
+      content: "To put it simply, a campaign is the collection of adventures and stories that your characters live! One dungeon master usually creates a campaign with its story for its players to enjoy."
+    },
+    {
+      imageURL: 'https://i.redd.it/lxbxr1xfssg51.png',
+      title: "What even is a campaign... ",
+      subtitle: " ...on DnDHUB",
+      content: "A campaign on dndhub is a way of connecting various players and one dungeon master to share infos about your own real life campaign."
+    },
+    {
+      imageURL: 'https://i.redd.it/0gj7i93lpq561.png',
+      title: "How to create a campaign",
+      subtitle: "Creation page",
+      content: "As soon as you'll go to the next page you will be greeted with some fields to fill, some of them are optional! You need at least a name for the campaign to create one. You can't own campaigns with the same name"
+    },
+    {
+      imageURL: 'https://www.dndspeak.com/wp-content/uploads/2021/03/Tavernkeepers-1.jpg',
+      title: "How to create a campaign",
+      subtitle: "At campaign creation",
+      content: "Once the campaign is created, you'll be transported to its own private campaign page. On here you will be able to post new information to your players, accept invitation and much more!"
     },
   ];
 
@@ -45,5 +63,6 @@ export class CampaignCreationInfoPage implements OnInit {
     );
   }
 
+  goBack = () => this.router.navigate(['/campaigns']);
   public createCampaign = () => this.router.navigate(["/campaign-creation"]);
 }
--- dndhub/src/app/pages/character-creation-info/character-creation-info.page.ts
+++ dndhub/src/app/pages/character-creation-info/character-creation-info.page.ts
@@ -28,19 +28,50 @@ export class CharacterCreationInfoPage implements OnInit {
   
   @Input() cards: Card[] = [
     {
-      imageURL: 'https://i.pinimg.com/1200x/3f/59/9b/3f599be10be14718b1cf5f0fb087f69f.jpg',
-      title: "Il mondo di Dungeons & Dragons è pieno di avventure...",
-      subtitle: "Non vorrai perdertele mica tutte!",
-      content: "Crea la tua campagna e potrai sbloccare infinite possibilità..."
+      imageURL: 'https://i.pinimg.com/1200x/68/9b/59/689b594cf67d0a37ada67687ea99bd33.jpg',
+      title: "Create a character... ",
+      subtitle: " ...with our tools.",
+      content: "And start playing!"
     },
     {
-      imageURL: 'https://i.pinimg.com/736x/f0/2c/72/f02c72a7cb29a8ceefc8fcea10f706c2.jpg',
-      title: "Immagina raccontare una storia e giocarla...",
-      subtitle: "Come vivere una favola!",
-      content: "Raduna i tuoi amici e affrontate orripilanti mostri nelle segrete più oscure della fantasia!"
+      imageURL: 'https://i.pinimg.com/736x/c9/22/7c/c9227c37b1a9e905863e3d1471c15278.jpg',
+      title: "Basic rules",
+      subtitle: "Why do I need a character",
+      content: "As a player, your character is the key to interact with the world and the stories created by your dungeon master."
+    },
+    {
+      imageURL: 'https://i.redd.it/7e1ewocvy8721.jpg',
+      title: "Basic rules",
+      subtitle: "How is character made: CLASS",
+      content: "Every character has a player class (and starting from level 3 a subclass) which determines the general fantasy and powers of your character. For example you could be a Wizard that studies magic from a young age or a Druid that loves and protects nature."
+    },
+    {
+      imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThouEoYKD-mRjAxqyOL4fSs6Tll2NA11JbB3pinOAyxQ&s=10',
+      title: "Basic rules",
+      subtitle: "How is character made: SPECIES",
+      content: "Every character has a species (and in some cases a subspecies) they belong to. The species describes, as the name intends, the type of humanoid the character is. Your character could be a noble elf or an hard-working dwarf!"
+    },
+    {
+      imageURL: 'https://i.pinimg.com/1200x/07/59/bc/0759bcbf392cc5dfce90b60260e3925f.jpg',
+      title: "Basic rules",
+      subtitle: "How is character made: BACKGROUND",
+      content: "Every character has a background, which describes the place they come from or the people that raised them."
+    },
+    {
+      imageURL: 'https://i.pinimg.com/1200x/6d/7e/a0/6d7ea0428a1541d8460681894e216dd0.jpg',
+      title: "Basic rules",
+      subtitle: "How is character made: STATISTICS",
+      content: "There are 6 statistics in the game: Strength, Dexterity, Constitution, Intelligence, Wisdom and Charisma. Each of them has to range from a minimum of 1 to a maximum of 20. Every statistic is tied to different abilities and checks and every class may prefer a statistic over another."
+    },
+    {
+      imageURL: 'https://i.pinimg.com/1200x/99/be/44/99be440beadd890dbf02cf75166a0ede.jpg',
+      title: "Basic rules",
+      subtitle: "How is character made: EQUIPMENT and SPELLS",
+      content: "As a closing note, every character has an equipment they carry with them. Some classes also know how to cast spells! The classes with spellcasting abilities are Bard, Cleric, Druid, Paladin, Ranger, Sorcerer, Warlock, Wizard."
     },
   ];
 
+  goBack = () => this.router.navigate(['/characters']);
   currentCard = signal<number>(this.cards[0] !== undefined ? 0 : -1);
 
   constructor(private router: Router) { }

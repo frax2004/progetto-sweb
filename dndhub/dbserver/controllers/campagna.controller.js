3f980601986e68a3e9c3232a2896ec18060991bd
Giovanni <nannibutera08@gmail.com>
2026-06-24 11:03:26 +0200

    css fix

--- dndhub/dbserver/controllers/campagna.controller.js
+++ dndhub/dbserver/controllers/campagna.controller.js
@@ -248,45 +248,6 @@ function createCampaignParticipationRequest(req,res) {
   }
 }
 
-export async function loadAcceptedPlayers(req, res) {
-  canSend = true;
-
-  const campaign_idx = req.body.campaign_idx;
-  const query = `
-  SELECT * FROM ArrayCampagnaPersonaggiItem
-  WHERE idx_campagna = '${campaign_idx}'
-  AND stato_personaggio = 'accepted'
-  `;
-
-  try {
-    const players_idx_rows = await Database.queryAll(query);
-
-    let players = [];
-    for(const row of players_idx_rows) {
-      const player_query = `SELECT * FROM Personaggio WHERE idx_personaggio = '${row.idx_personaggio}'`;
-      const player = await Database.queryOne(player_query);
-      players.push(player);
-    }
-
-    sendResponse({
-        message: "Giocatori ottenuti con successo",
-        status_code: 200,
-        success: true,
-        players: players
-      },
-      res
-    );
-  } catch(err) {
-    sendResponse({
-        status_code: 401,
-        success: false,
-        message: "Impossibile ottenere i personaggi: " + err
-      },
-      res
-    );
-  }
-}
-
 
 export async function loadCampaignPlayers(req, res) {
   canSend = true;
